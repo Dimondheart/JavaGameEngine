da536eeb296d419bfaa1014b9657101e8e24ec93
Bryan Bettis <bryancbettis@gmail.com>
2015-11-13 21:18:09 -0600

    BGM Looping
    BGM now loops indefinitely.

--- src/main/sound/BGM.java
+++ src/main/sound/BGM.java
@@ -1,10 +1,13 @@
 package main.sound;
 
+import javax.sound.sampled.Clip;
+
 public class BGM extends Sound
 {
 	public BGM(String track, Volume volume)
 	{
 		super("bgm/" + track, volume);
+		this.clip.loop(Clip.LOOP_CONTINUOUSLY);
 	}
 	
 	/** Stops this track from playing. */
